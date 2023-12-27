b3dab252e478867038ea204d3717c28d0217e230
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-12-27 17:23:31 +0530

    BacktoTop button is added

---
+++ src/Components/BackToTopBtn.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import arrowUp from "../../public/images/svg-icons/arrow-up.svg";
+
+const BackToTopBtn = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    const scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    setIsVisible(scrollTop > 30);
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <>
+      {isVisible && (
+        <button id="topbtn" onClick={scrollToTop}>
+          <img src={arrowUp} alt="" />
+        </button>
+      )}
+    </>
+  );
+};
+
+export default BackToTopBtn;
