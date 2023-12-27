e12f5a0c95311d259fdda4223199b81935c5e9f3
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-12-27 23:28:03 +0530

    About page is added

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,38 +1,44 @@
 import React from "react";
 import "./About.css";
+import about from "../../../public/images/profile/about-img.png";
 
 const About = () => {
   return (
     <>
-      <div className="About min-h-[100vh] gap-5 " id="about">
+      <div className="About min-h-[90vh] gap-5 " id="about">
         <h1 className="text-5xl font-bold text-center">About me</h1>
-        <div className="content1 flex justify-around mt-[2rem] h-fit">
-          <div className="ctxt  w-[100%] mt-6">
-            <p className="text-justify">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Cupiditate, voluptatum quam perferendis sapiente cumque eos
-              commodi corrupti esse consectetur saepe accusantium ea quas quos
-              aut tenetur? Quaerat architecto, sapiente quae modi nobis ad, odio
-              facere minima numquam provident harum incidunt. Lorem ipsum dolor
-              sit amet consectetur adipisicing elit. Ut id, ad officia est
-              perferendis dolore dignissimos quam. Molestias enim id eius
-              perspiciatis odit voluptatem illum doloribus repudiandae ipsa,
-              tempora commodi voluptatum error aperiam, corporis itaque
-              accusamus eos recusandae earum temporibus velit dolore cupiditate
-              sequi? Cumque odit nemo, iure quasi recusandae in doloremque
-              dolorem est mollitia quo perspiciatis amet debitis rem quia
-              nesciunt, deleniti, cum tempora dignissimos. Temporibus, quam
-              consequuntur. Ex ab corrupti qui dolores sed totam et ea
-              perspiciatis repellendus suscipit, praesentium deserunt recusandae
-              possimus culpa accusantium voluptatibus asperiores enim molestias
-              exercitationem eos dolor a. Quis, commodi? Et, esse quia?.
+        <div className="borderBottom w-[100%] m-auto ">
+          <div className="border2"></div>
+          <div className="border1 "></div>
+          <div className="border2"></div>
+        </div>
+        <div className="content1 flex justify-around mt-[2rem] h-[100%]">
+          <div className="ctxt  w-[50%] mt-6">
+            <p className="text-justify p-3 text-[1.1rem]">
+              Hello, I'm <b>Aniket</b>, a final-year student currently pursuing
+              Computer Engineering at Government Engineering College
+              Gandhinagar. I specialize in Full Stack Web Development and have a
+              keen interest in strengthening my mathematical fundamentals.
+              <br />
+              <br />
+              My core skills in Front-End development, HTML, CSS, SCSS,
+              JavaScript, and React-JS. Additionally, I am proficient in
+              utilizing front-end frameworks such as Bootstrap and Tailwind CSS.
+              On the back-end, I work with PHP and MySQL to create robust and
+              dynamic applications. I have successfully completed several
+              projects utilizing these technologies, showcasing my ability to
+              deliver high-quality work.
+              <br />
+              <br />I enjoy about learning new technologies and constantly
+              strive to expand my skill set. I am eager to collaborate on live
+              projects, bringing my expertise.
             </p>
           </div>
-          <div className="right items-center justify-center mt-[2rem] aboutimage">
+          <div className="right items-center justify-center mt-[0.5rem] aboutimage w-[25%] text-center">
             <img
-              src="./images/photo_2020-12-05_18-43-24.jpg"
+              src={about}
               alt=""
-              className="w-[28rem] h-[28rem] rounded-xl aboutImg"
+              className="w-[100%] m-auto rounded-xl aboutImg"
             />
           </div>
         </div>
