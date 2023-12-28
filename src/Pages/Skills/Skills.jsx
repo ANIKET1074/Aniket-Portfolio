9bb49cdcd67919f1dec3837338228ed52d3b9e11
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-12-28 10:50:15 +0530

    Skills page is added

--- src/Pages/Skills/Skills.jsx
+++ src/Pages/Skills/Skills.jsx
@@ -9,6 +9,11 @@ const Skills = () => {
     <>
       <div id="skills" className="skills ">
         <h1 className="font-bold text-5xl text-center">Skills</h1>
+        <div className="borderBottom w-[100%] m-auto ">
+          <div className="border2"></div>
+          <div className="border1 "></div>
+          <div className="border2"></div>
+        </div>
         <div className="flex flex-wrap gap-11 mx-auto py-[1rem] mt-[1.5rem] w-[95%]  rounded-xl bg-transparent skill">
           <div className="slider overflow-hidden px-[2rem]">
             <div className="slide-wrapper flex gap-[2rem] bg-transparent">
